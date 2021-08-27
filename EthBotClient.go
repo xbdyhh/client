35f4a48672c435f0eefc416e60a063a3d9f94c1c
panxinyang <panxinyang@nodes.wiki>
2021-08-27 16:07:49 +0800

    get

--- EthBotClient.go
+++ EthBotClient.go
@@ -1,15 +1,17 @@
 package go_http_client
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 )
 
 type  EthInteractiveBotClient struct {
-	Url string
-	UrlData map[string]string
-	Data map[interface{}]interface{}
-	Response *http.Response
+	Url string//域名网址
+	UrlData map[string]string//网址中需要的搜索字段
+	Data map[interface{}]interface{}//使用POST方法时的传输字段
+	Response []byte//返回值
 }
 
 func NewEthINteractiveBotClient() *EthInteractiveBotClient {
@@ -24,8 +26,10 @@ func NewEthINteractiveBotClient() *EthInteractiveBotClient {
 
 func (ECli *EthInteractiveBotClient)Get() error{
 	var data  url.Values
-	for k,v := range ECli.UrlData{
-		data.Set(k,v)
+	if ECli.UrlData != nil{
+		for k,v := range ECli.UrlData{
+			data.Set(k,v)
+		}
 	}
 	rawQuery := data.Encode()
 	u, err := url.ParseRequestURI(ECli.Url)
@@ -34,9 +38,16 @@ func (ECli *EthInteractiveBotClient)Get() error{
 	}
 	u.RawQuery = rawQuery
 	resp, err := http.Get(u.String())
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil{
 		return err
 	}
-	ECli.Response = resp
+	ECli.Response = body
+	if resp.Body == nil{
+		fmt.Print("body is nil")
+	}else {
+		fmt.Printf("body is %v",string(body))
+	}
 	return nil
 }
--- EthBotClient_test.go
+++ EthBotClient_test.go
@@ -1,1 +1,17 @@
 package go_http_client
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewEthINteractiveBotClient(t *testing.T) {
+	cli := NewEthINteractiveBotClient()
+	cli.Url = "http://www.baidu.com/"
+	cli.Get()
+	if cli.Response == nil{
+		t.Errorf("eth bot get err")
+	}else {
+		fmt.Printf("response is :%v",string(cli.Response))
+	}
+}
