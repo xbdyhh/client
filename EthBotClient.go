e53d646d27e0861bdf0a90a0490958bf6e32707c
panxinyang <1670016749@qq.com>
2021-09-08 21:35:29 +0800

    fix_get_method

--- EthBotClient.go
+++ EthBotClient.go
@@ -1,89 +1,50 @@
 package client
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 type  EthInteractiveBotClient struct {
-	Url string//域名网址
-	ContentType string
-	UrlData map[string]string//网址中需要的搜索字段
-	Data map[interface{}]interface{}//使用POST方法时的传输字段
-	Response []byte//返回值
+	http.Client
+	Request *http.Request
+	Query url.Values//url中的query
+	ResponseBody []byte
+
 }
 
 func NewEthINteractiveBotClient() *EthInteractiveBotClient {
 	return &EthInteractiveBotClient{
-		Url:      "",
-		ContentType: "application/json",
-		UrlData: nil,
-		Data:     nil,
-		Response: nil,
+		ResponseBody: nil,
 	}
+}
+
+func (ecli *EthInteractiveBotClient)InitUrl(host,scheme,path,fragment string,Query map[string][]string)  {
+	termUrl := new(url.URL)
+	termUrl.Host =  host
+	termUrl.Scheme = scheme
+	termUrl.Path = path
+	termUrl.Fragment = fragment
+	ecli.Request.URL =termUrl
+	ecli.Query = Query
 }
 
 
 func (ECli *EthInteractiveBotClient)Get() error{
-	var data  url.Values
-	if ECli.UrlData != nil{
-		for k,v := range ECli.UrlData{
-			data.Set(k,v)
-		}
-	}
-	rawQuery := data.Encode()
-	u, err := url.ParseRequestURI(ECli.Url)
-	if err != nil{
-		return err
-	}
-	u.RawQuery = rawQuery
-	resp, err := http.Get(u.String())
+	ECli.Request.URL.RawQuery = ECli.Query.Encode()
+	resp,err := ECli.Do(ECli.Request)
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil{
 		return err
 	}
-	ECli.Response = body
+	ECli.ResponseBody = body
 	if resp.Body == nil{
 		fmt.Print("body is nil")
 	}else {
 		fmt.Printf("body is %v",string(body))
-	}
-	return nil
-}
-
-func (ECli *EthInteractiveBotClient)Post()error{
-	var data  url.Values
-	if ECli.UrlData != nil{
-		for k,v := range ECli.UrlData{
-			data.Set(k,v)
-		}
-	}
-	rawQuery := data.Encode()
-	u, err := url.ParseRequestURI(ECli.Url)
-	if err != nil{
-		return err
-	}
-	u.RawQuery = rawQuery
-	s,err :=json.Marshal(ECli.Data)
-	if err != nil{
-		return err
-	}
-	resp,err :=http.Post(u.String(),ECli.ContentType,strings.NewReader(string(s)))
-	if err != nil{
-		return err
-	}
-	if resp.StatusCode!= 200{
-		return errors.New("connect err happened！！！！")
-	}
-	ECli.Response,err = ioutil.ReadAll(resp.Body)
-	if err != nil{
-		return err
 	}
 	return nil
 }
